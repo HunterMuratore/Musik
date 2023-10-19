eecf6bae882836e5be15e689f7e4ca4ec4f8b519
Hunter <muratoreh@gmail.com>
2023-10-19 13:44:24 -0400

    fixed images - WIP spotify api calls

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,15 @@
-const spotifyUrl = 'https://spotify23.p.rapidapi.com/tracks/?ids=4WNcduiCmDNfmTEz7JvmLv';
+// client id- 43743c2db80a47e19d719147c8da72dc
+//client secret- 5f7fe806eccd4cccb17c7be733249cae
+
+const rapidUrl = 'https://spotify23.p.rapidapi.com/tracks/?ids=4WNcduiCmDNfmTEz7JvmLv';
+const spotifyUrl = 'https://api.spotify.com/v1/browse/categories';
 
 async function fetchSpotifyData() {
     try {
         const response = await fetch(spotifyUrl, {
             'method': 'GET',
             'headers': {
-                'X-RapidAPI-Key': '88103c9392mshb329b0956793917p1dcdb6jsnde85ccce0f0c',
-                'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+                Authorization: `Bearer ${token}`
             }
         });
 
@@ -24,3 +27,24 @@ async function fetchSpotifyData() {
 }
 
 fetchSpotifyData();
+
+// try {
+//     const response = await fetch(spotifyUrl, {
+//         'method': 'GET',
+//         'headers': {
+//             'X-RapidAPI-Key': '88103c9392mshb329b0956793917p1dcdb6jsnde85ccce0f0c',
+//             'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+//         }
+//     });
+
+//     if (response.ok) {
+//         const result = await response.text();
+//         console.log(result);
+//         return result;
+//     }
+
+//     console.log('An error occured with the API');
+
+// } catch (err) {
+//     console.log(err);
+// }
