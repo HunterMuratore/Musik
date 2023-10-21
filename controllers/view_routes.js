a10a66603283d5de5d089dfc719b5887e63d76e3
Alex Pohlman <alexpohlman@Alexs-MacBook-Pro.local>
2023-10-21 17:58:24 -0400

    added artist name and album cover to songs.hbs

--- controllers/view_routes.js
+++ controllers/view_routes.js
@@ -1,88 +1,93 @@
-const router = require('express').Router();
-const User = require('../models/User');
-const Post = require('../models/Post');
-const { authenticate, isLoggedIn, isAuthenticated } = require('../middleware/authenticate');
-const { getSongsByTitle } = require('../queries/get_song');
-
+const router = require("express").Router();
+const User = require("../models/User");
+const Post = require("../models/Post");
+const {
+  authenticate,
+  isLoggedIn,
+  isAuthenticated,
+} = require("../middleware/authenticate");
+const { getSongsByTitle } = require("../queries/get_song");
 
 /* / routes */
 
 // Root page to show Musik posts
-router.get('/', authenticate, async (req, res) => {
-    // Find all of the Posts
-    const posts = await Post.findAll({
-        include: {
-            model: User,
-            as: 'author'
-        }
-    });
+router.get("/", authenticate, async (req, res) => {
+  // Find all of the Posts
+  const posts = await Post.findAll({
+    include: {
+      model: User,
+      as: "author",
+    },
+  });
 
-    res.render('home', { 
-        user: req.user,
-        // Send back only the plain objects for the posts array
-        posts: posts.map(p => p.get({ plain: true }))
-    });
+  res.render("home", {
+    user: req.user,
+    // Send back only the plain objects for the posts array
+    posts: posts.map((p) => p.get({ plain: true })),
+  });
 });
 
 // Show the register form if the user is not logged in
-router.get('/register', isLoggedIn, authenticate, (req, res) => {
-    res.render('register', {
-        // If there are any session errors from them trying to register then they will be sent through to this page for us to use
-        errors: req.session.errors,
-        user: req.user
-    });
+router.get("/register", isLoggedIn, authenticate, (req, res) => {
+  res.render("register", {
+    // If there are any session errors from them trying to register then they will be sent through to this page for us to use
+    errors: req.session.errors,
+    user: req.user,
+  });
 
-    // Clear the error array after you render them
-    req.session.errors = [];
+  // Clear the error array after you render them
+  req.session.errors = [];
 });
 
 // Show the login form if the user is not logged in
-router.get('/login', isLoggedIn, authenticate, (req, res) => {
-    res.render('login', {
-        errors: req.session.errors,
-        user: req.user
-    });
+router.get("/login", isLoggedIn, authenticate, (req, res) => {
+  res.render("login", {
+    errors: req.session.errors,
+    user: req.user,
+  });
 
-    req.session.errors = [];
+  req.session.errors = [];
 });
 
 // Show profile only if the user is authenticated
-router.get('/profile', isAuthenticated, authenticate, async (req, res) => {    
-    const user_id = req.session.user_id;
-    // Find all of the Posts by the user
-    const posts = await Post.findAll({
-        where: {
-            author_id: user_id
-        },
-        include: {
-            model: User,
-            as: 'author'
-        }
-    });
-    
-    res.render('profile', {
-        user: req.user,
-        posts: posts.map(p => p.get({ plain: true }))
-    });
+router.get("/profile", isAuthenticated, authenticate, async (req, res) => {
+  const user_id = req.session.user_id;
+  // Find all of the Posts by the user
+  const posts = await Post.findAll({
+    where: {
+      author_id: user_id,
+    },
+    include: {
+      model: User,
+      as: "author",
+    },
+  });
 
-    req.session.errors = [];
+  res.render("profile", {
+    user: req.user,
+    posts: posts.map((p) => p.get({ plain: true })),
+  });
+
+  req.session.errors = [];
 });
 
-router.get('/songs', isAuthenticated, authenticate, async(req, res) => {
-    try {
-        const track = req.query.id;
-        const userComment = req.query.comment;
-        const { items } = await getSongsByTitle(track);
-        
-        res.render('songs', {
-            tracks: items,
-            comment: encodeURIComponent(userComment)
-        });
-    } catch (err) {
-        // Handle any errors that may occur during the Spotify API request.
-        console.error(err);
-        res.redirect('/profile');
-    }
+router.get("/songs", isAuthenticated, authenticate, async (req, res) => {
+  try {
+    const track = req.query.id;
+    const userComment = req.query.comment;
+    const { items } = await getSongsByTitle(track);
+
+    console.log(items[0]);
+
+    res.render("songs", {
+      tracks: items,
+      comment: encodeURIComponent(userComment),
+    });
+  } catch (err) {
+    // Handle any errors that may occur during the Spotify API request.
+    console.error(err);
+    res.redirect("/profile");
+  }
 });
 
 // Export the router
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const view_routes = require('./controllers/view_routes');
-const createPost_routes = require('./controllers/createPost_routes');
-const user_routes = require('./controllers/user_routes');
-const song_routes = require('./controllers/song_routes');
-const db = require('./config/connection');
-const { engine } = require('express-handlebars');
-const session = require('express-session');
-require('dotenv').config();
+const express = require("express");
+const view_routes = require("./controllers/view_routes");
+const createPost_routes = require("./controllers/createPost_routes");
+const user_routes = require("./controllers/user_routes");
+const song_routes = require("./controllers/song_routes");
+const db = require("./config/connection");
+const { engine } = require("express-handlebars");
+const session = require("express-session");
+require("dotenv").config();
 
 const PORT = process.env.PORT || 3333;
 
@@ -14,23 +14,24 @@ const app = express();
 
 // Open Middleware channels
 /* Handlebars Middleware */
-app.engine('.hbs', engine({ extname: '.hbs' }));
-app.set('view engine', '.hbs');
+app.engine(".hbs", engine({ extname: ".hbs" }));
+app.set("view engine", ".hbs");
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 /* Load Session Middleware */
-app.use(session({
+app.use(
+  session({
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: true
-}));
+    saveUninitialized: true,
+  })
+);
 
 // Open routes at the root level
-app.use('/', view_routes, createPost_routes, song_routes);
-app.use('/auth', user_routes);
+app.use("/", view_routes, createPost_routes, song_routes);
+app.use("/auth", user_routes);
 
 // Sync and create tables
-db.sync({ force: false })
-    .then(() => {
-        app.listen(PORT, () => console.log('Server started on port', PORT));
-    });
+db.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log("Server started on port", PORT));
+});
