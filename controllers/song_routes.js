11bf8ce032eb566e717fba11c935a42bf9cf700d
Alex Pohlman <alexpohlman@Alexs-MacBook-Pro.local>
2023-10-22 19:32:45 -0400

    changed home.hbs to display the actual image instead of the link, add some tailwind to size the images

--- controllers/song_routes.js
+++ controllers/song_routes.js
@@ -1,37 +1,36 @@
-const router = require('express').Router();
-const { getSong } = require('../queries/get_song');
-const Post = require('../models/Post');
-const { authenticate, isAuthenticated } = require('../middleware/authenticate');
+const router = require("express").Router();
+const { getSong } = require("../queries/get_song");
+const Post = require("../models/Post");
+const { authenticate, isAuthenticated } = require("../middleware/authenticate");
 
+router.get("/song", isAuthenticated, authenticate, async (req, res) => {
+  const id = req.query.id;
+  const comment = req.query.comment;
+  const song = await getSong(id);
 
-router.get('/song', isAuthenticated, authenticate, async (req, res) => {
-    const id = req.query.id;
-    const comment = req.query.comment;
-    const song = await getSong(id);
+  const songData = {
+    track: song.name,
+    artist: song.artists[0].name,
+    album_cover: song.album.images[0].url,
+    album_name: song.album.name,
+    comment: comment,
+  };
 
-    const songData = {
-        track: song.name,
-        artist: song.artists[0].name,
-        album_cover: song.album.images[0].url,
-        album_name: song.album.name,
-        comment: comment
+  // Create a post
+  try {
+    const newPost = await Post.create(songData);
+
+    // Add the Post to the user in the session
+    await req.user.addPost(newPost);
+
+    res.redirect("/profile");
+  } catch (err) {
+    console.log(err);
+    if (err.errors) {
+      req.session.errors = err.errors.map((errObj) => errObj.message);
     }
-
-    // Create a post
-    try {
-        const newPost = await Post.create(songData);
-
-        // Add the Post to the user in the session
-        await req.user.addPost(newPost);
-
-        res.redirect('/profile');
-    } catch (err) {
-        console.log(err);
-        if (err.errors) {
-            req.session.errors = err.errors.map(errObj => errObj.message);
-        }
-        res.redirect('/profile');
-    }
+    res.redirect("/profile");
+  }
 });
 
 module.exports = router;
